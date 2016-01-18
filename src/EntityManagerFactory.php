2b9a3536097259d734e25da28d48f42a65f2eb42
Lucaszz <lucaszz111@gmail.com>
2016-01-18 17:44:49 +0100

    EntityManagerFactory introduced

---
+++ src/EntityManagerFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Lucaszz\TestsWithDatabaseExamples;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\ORM\Tools\Setup;
+
+final class EntityManagerFactory
+{
+    public static function create(array $params)
+    {
+        $entityPath = [__DIR__.'/Entity'];
+        $config     = Setup::createAnnotationMetadataConfiguration($entityPath, false);
+        $driver     = new AnnotationDriver(new AnnotationReader(), $entityPath);
+        AnnotationRegistry::registerLoader('class_exists');
+        $config->setMetadataDriverImpl($driver);
+
+        return EntityManager::create($params, $config);
+    }
+}
---
+++ tests/EntityManagerFactoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lucaszz\TestsWithDatabaseExamples\Tests;
+
+use Doctrine\ORM\EntityManager;
+use Lucaszz\TestsWithDatabaseExamples\EntityManagerFactory;
+
+class EntityManagerFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_creates_instance_of_entity_manager()
+    {
+        $this->assertInstanceOf(
+            EntityManager::class,
+            EntityManagerFactory::create(
+                [
+                    'driver'   => 'pdo_mysql',
+                    'user'     => 'root',
+                    'password' => '',
+                    'dbname'   => 'database-test',
+                ]
+            )
+        );
+    }
+}
