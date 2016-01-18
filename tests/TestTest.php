b0aedb8ccf3fabbc45c74af3b137436e880d882e
Lucaszz <lucaszz111@gmail.com>
2016-01-18 11:56:46 +0100

    Project setup

---
+++ tests/TestTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Lucaszz\TestsWithDatabaseExamples\Tests;
+
+class TestTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
